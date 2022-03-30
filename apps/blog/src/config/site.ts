4f747b8afc742fa8c322125d4fecd1496230d33a
Rendi Rizki <rendirizkir@gmail.com>
2022-03-30 14:35:29 +0700

    chore: blog templating

---
+++ apps/blog/src/config/site.ts
@@ -0,0 +1,9 @@
+export const SITE = {
+  name: 'Blog',
+  title: 'Rendi Riz Blog',
+  description: 'Crisp, minimal, personal website theme for Astro',
+  url: 'https://astro-boilerplate.vercel.app/',
+  githubUrl: 'https://github.com/rendiriz/astro-boilerplate/',
+};
+
+export const PAGE_SIZE = 2;
