7f18a4107fb9bdddee02621385c87128eb02d1b0
Rendi Rizki <rendirizkir@gmail.com>
2022-04-04 23:20:33 +0700

    feat(personal): blog search

--- apps/personal/src/templates/sidebar/BlogRight.tsx
+++ apps/personal/src/templates/sidebar/BlogRight.tsx
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import { EuiTitle } from '@elastic/eui';
+import {
+  EuiLoadingSpinner,
+  EuiModal,
+  EuiModalBody,
+  EuiModalHeader,
+  EuiTitle,
+} from '@elastic/eui';
 import { IKImage, IKContext } from 'imagekitio-react';
+import { useQuery } from 'react-query';
+
 import FooterBlog from '@/templates/footer/Blog';
+import PostPreviewList from '@/components/PostPreviewList';
+import useDebounce from '@/hooks/useDebounce';
 
 const EuiFieldText = dynamic(
   () => import('@elastic/eui').then((mod) => mod.EuiFieldText),
@@ -11,12 +21,77 @@ const EuiFieldText = dynamic(
   },
 );
 
-function BlogRight() {
-  const [value, setValue] = useState('');
+const getPosts = async (search): Promise<any> => {
+  const res = await fetch(`/api/search?q=${search}`);
+  return res.json();
+};
 
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
+function BlogRight() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const { data, error, status } = useQuery<any, Error>(
+    ['posts', debouncedSearchTerm],
+    () => getPosts(debouncedSearchTerm),
+    {
+      enabled: isSearching,
+    },
+  );
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      setIsSearching(true);
+    } else {
+      setIsSearching(false);
+    }
+  }, [debouncedSearchTerm]);
+
+  const closeModal = () => setIsModalVisible(false);
+  const showModal = () => setIsModalVisible(true);
+
+  let modal;
+
+  if (isModalVisible) {
+    modal = (
+      <EuiModal style={{ width: 800 }} onClose={closeModal}>
+        <EuiModalHeader />
+
+        <EuiModalBody>
+          <EuiFieldText
+            placeholder="What are you looking for?"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            aria-label="Search"
+            fullWidth
+          />
+          <div className="mt-24px">
+            {status === 'loading' ? (
+              <div className="block flex justify-center mt-48px mb-36px">
+                <EuiLoadingSpinner size="xxl" />
+              </div>
+            ) : status === 'error' ? (
+              <div className="block flex justify-center mt-48px mb-36px">
+                <p>Error: {error.message}</p>
+              </div>
+            ) : (
+              <div>
+                {data && data.length > 0 ? (
+                  <PostPreviewList posts={data} closeModal={closeModal} />
+                ) : (
+                  <div className="block flex justify-center mt-48px mb-36px">
+                    <p>No results found</p>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </EuiModalBody>
+      </EuiModal>
+    );
+  }
 
   return (
     <div className="hidden w-394px min-h-screen px-32px xl:block">
@@ -26,10 +101,12 @@ function BlogRight() {
             <div className="block pt-40px">
               <EuiFieldText
                 placeholder="Search"
-                value={value}
-                onChange={(e) => onChange(e)}
-                aria-label="Use aria labels when no actual label is in use"
+                onClick={showModal}
+                aria-label="Search"
+                className="!cursor-pointer"
+                readOnly
               />
+              {modal}
             </div>
 
             <div className="block">
